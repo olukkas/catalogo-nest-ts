9294ccd7f21e612bf9d2b5a9c6b9892435cf10e7
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-04-06 22:31:43 -0300

    test(categories): helper class for categories integration tests
    the InjectorHelper class helps to create instances of useCases for testing purposes

---
+++ src/nestjs/src/categories/__tests__/injector.helper.ts
@@ -0,0 +1,32 @@
+import { CategoriesController } from '../categories.controller';
+import { CategoryRepository } from '@fc/micro-videos/category/domain';
+import {
+  CreateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from '@fc/micro-videos/category/application';
+
+export type Initializer = {
+  name: string;
+  useCase: any;
+};
+
+export class InjectorHelper {
+  constructor(private repo: CategoryRepository.Repository) {}
+
+  initUseCases(cases: Initializer[], controller: CategoriesController,) {
+    for (const { name, useCase } of cases) {
+      controller[name] = new useCase(this.repo);
+    }
+  }
+}
+
+export const DEFAULT_USE_CASES = [
+  { name: 'createUseCase', useCase: CreateCategoryUseCase.UseCase },
+  { name: 'updateUseCase', useCase: UpdateCategoryUseCase.UseCase },
+  { name: 'deleteUseCase', useCase: DeleteCategoryUseCase.UseCase },
+  { name: 'getUseCase', useCase: GetCategoryUseCase.UseCase },
+  { name: 'listUseCase', useCase: ListCategoriesUseCase.UseCase },
+];
--- src/nestjs/src/categories/__tests__/integration/categories.controller-int.spec.ts
+++ src/nestjs/src/categories/__tests__/integration/categories.controller-int.spec.ts
@@ -3,19 +3,50 @@ import { Test } from '@nestjs/testing';
 import { ConfigModule } from '../../../config/config.module';
 import { DatabaseModule } from '../../../database/database.module';
 import { CategoriesModule } from '../../categories.module';
+import { DEFAULT_USE_CASES, InjectorHelper } from '../injector.helper';
+import {
+  CreateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from '@fc/micro-videos/category/application';
+import { CATEGORY_PROVIDERS } from '../../category.providers';
+import { CategoryRepository } from '@fc/micro-videos/category/domain';
 
 describe('CategoriesController integration tests', function () {
   let controller: CategoriesController;
+  let repository: CategoryRepository.Repository;
 
   beforeEach(async () => {
     const modulo = await Test.createTestingModule({
       imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
     }).compile();
 
+    repository = modulo.get(
+      CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+    );
+
+    const helper = new InjectorHelper(repository);
+
     controller = modulo.get(CategoriesController);
+    helper.initUseCases(DEFAULT_USE_CASES, controller);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(controller['getUseCase']).toBeInstanceOf(GetCategoryUseCase.UseCase);
+    expect(controller['createUseCase']).toBeInstanceOf(
+      CreateCategoryUseCase.UseCase,
+    );
+    expect(controller['updateUseCase']).toBeInstanceOf(
+      UpdateCategoryUseCase.UseCase,
+    );
+    expect(controller['deleteUseCase']).toBeInstanceOf(
+      DeleteCategoryUseCase.UseCase,
+    );
+    expect(controller['listUseCase']).toBeInstanceOf(
+      ListCategoriesUseCase.UseCase,
+    );
   });
 });
