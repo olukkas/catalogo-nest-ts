0ca552bf76af5b60bc63e46479b608b48a6ea6bb
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-04-07 12:40:40 -0300

    test(categories): update categories integration test

--- src/nestjs/src/categories/__tests__/integration/categories.controller-int.spec.ts
+++ src/nestjs/src/categories/__tests__/integration/categories.controller-int.spec.ts
@@ -13,11 +13,13 @@ import {
 } from '@fc/micro-videos/category/application';
 import { CATEGORY_PROVIDERS } from '../../category.providers';
 import { CategoryRepository } from '@fc/micro-videos/category/domain';
-import { text } from 'express';
+import { CategorySequelize } from '@fc/micro-videos/category/infra';
+import CategoryModel = CategorySequelize.CategoryModel;
 
 describe('CategoriesController integration tests', function () {
   let controller: CategoriesController;
   let repository: CategoryRepository.Repository;
+  const factory = CategorySequelize.CategoryModel.factory();
 
   beforeEach(async () => {
     const modulo = await Test.createTestingModule({
@@ -80,16 +82,77 @@ describe('CategoriesController integration tests', function () {
     test.each(arrange)(
       'with request $request',
       async ({ request, expectedOutput }) => {
-        const output = await controller.create(request);
-        const entity = await repository.findById(output.id);
+        const presenter = await controller.create(request);
+        const entity = await repository.findById(presenter.id);
 
         expect(entity).toMatchObject({
-          id: output.id,
+          id: presenter.id,
           name: expectedOutput.name,
           description: expectedOutput.description,
           is_active: expectedOutput.is_active,
-          created_at: output.created_at,
+          created_at: presenter.created_at,
         });
+
+        expect(presenter.id).toBe(entity.id);
+        expect(presenter.name).toBe(expectedOutput.name);
+        expect(presenter.description).toBe(expectedOutput.description);
+        expect(presenter.is_active).toBe(expectedOutput.is_active);
+        expect(presenter.created_at).toStrictEqual(entity.created_at);
+      },
+    );
+  });
+
+  describe('should update a category', function () {
+    let category: CategoryModel;
+
+    beforeEach(async () => {
+      category = await factory.create();
+    });
+
+    const arrange = [
+      {
+        request: {
+          name: 'Movie',
+        },
+        expectedOutput: {
+          name: 'Movie',
+          description: null,
+          is_active: true,
+        },
+      },
+      {
+        request: {
+          name: 'Séries',
+          description: null,
+          is_active: false,
+        },
+        expectedOutput: {
+          name: 'Séries',
+          description: null,
+          is_active: false,
+        },
+      },
+    ];
+
+    test.each(arrange)(
+      'with request $request',
+      async ({ request, expectedOutput }) => {
+        const presenter = await controller.update(category.id, request);
+        const entity = await repository.findById(category.id);
+
+        expect(entity).toMatchObject({
+          id: presenter.id,
+          name: expectedOutput.name,
+          description: expectedOutput.description,
+          is_active: expectedOutput.is_active,
+          created_at: presenter.created_at,
+        });
+
+        expect(presenter.id).toBe(entity.id);
+        expect(presenter.description).toBe(expectedOutput.description);
+        expect(presenter.name).toBe(expectedOutput.name);
+        expect(presenter.is_active).toBe(expectedOutput.is_active);
+        expect(presenter.created_at).toStrictEqual(entity.created_at);
       },
     );
   });
