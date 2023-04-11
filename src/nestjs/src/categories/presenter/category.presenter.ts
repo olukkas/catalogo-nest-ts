f3dbd4e89765e23f117c597dc7f8db7b45d0b79f
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-04-10 22:02:15 -0300

    feat(categories): add pagination and collection presenter

--- src/nestjs/src/categories/presenter/category.presenter.ts
+++ src/nestjs/src/categories/presenter/category.presenter.ts
@@ -1,5 +1,8 @@
-import { CategoryOutput } from '@fc/micro-videos/category/application';
-import { Transform } from 'class-transformer';
+import {
+  CategoryOutput,
+  ListCategoriesUseCase,
+} from '@fc/micro-videos/category/application';
+import { Transform, Exclude, Expose } from 'class-transformer';
 
 export class CategoryPresenter {
   id: string;
@@ -16,5 +19,59 @@ export class CategoryPresenter {
     this.description = output.description;
     this.is_active = output.is_active;
     this.created_at = output.created_at;
+  }
+}
+
+export type PaginationProps = {
+  current_page: number;
+  per_page: number;
+  last_page: number;
+  total: number;
+};
+
+export class PaginationPresenter {
+  @Transform(({ value }) => parseInt(value))
+  current_page: number;
+
+  @Transform(({ value }) => parseInt(value))
+  per_page: number;
+
+  @Transform(({ value }) => parseInt(value))
+  last_page: number;
+
+  @Transform(({ value }) => parseInt(value))
+  total: number;
+
+  constructor(props: PaginationProps) {
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.last_page = props.last_page;
+    this.total = props.total;
+  }
+}
+
+export abstract class CollectionPresenter {
+  @Exclude()
+  protected paginationPresenter: PaginationPresenter;
+
+  constructor(props: PaginationProps) {
+    this.paginationPresenter = new PaginationPresenter(props);
+  }
+
+  @Expose({ name: 'meta' })
+  get meta() {
+    return this.paginationPresenter;
+  }
+
+  // abstract get data();
+}
+
+export class CategoryCollectionPresenter extends CollectionPresenter {
+  data: CategoryPresenter[];
+
+  constructor(output: ListCategoriesUseCase.Output) {
+    const { items, ...props } = output;
+    super(props);
+    this.data = items.map((item) => new CategoryPresenter(item));
   }
 }
